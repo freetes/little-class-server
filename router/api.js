966ac60ec58679fb2e55a2cfbe36936c75dc56a2
freetes'Mac <freetes@foxmail.com>
2019-04-17 08:47:02 +0800

    修复请求方法；

--- router/api.js
+++ router/api.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const api = require('../controller/Api');
 
 /* GET api page. */
-router.get('/login', api.login);
-router.get('/setUserInfo', api.setUserInfo);
+router.post('/login', api.login);
+router.post('/setUserInfo', api.setUserInfo);
 
 module.exports = router;
