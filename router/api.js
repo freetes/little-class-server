ccb3c471060b3b9c88b6a33f76ae5ea691a4b52e
freetes'Mac <freetes@foxmail.com>
2019-04-20 21:52:51 +0800

    修复路由配置；

--- router/api.js
+++ router/api.js
@@ -6,6 +6,10 @@ router.post('/login', api.login);
 router.post('/setUserInfo', api.setUserInfo);
 
 router.post('/createGroup', api.createGroup);
-
+router.post('/getGroupInfoById', api.getGroupInfoById);
+router.post('/joinGroupByGroupId', api.joinGroupByGroupId);
+router.post('/exitGroupByGroupId', api.exitGroupByGroupId);
+router.post('/disbandGroupByGroupId', api.disbandGroupByGroupId);
+router.post('/getAllGroupsByUserId', api.getAllGroupsByUserId);
 
 module.exports = router;
